693ab1d42c6116073b1a768bb5aa105a508bb4f0
Jorgnavarro <jorgluisnavarro@gmail.com>
2024-02-10 11:08:25 -0500

    updates

--- anecdotes-redux/src/App.jsx
+++ anecdotes-redux/src/App.jsx
@@ -10,7 +10,7 @@ import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 function App() {
   const dispatch = useDispatch()
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(initializeAnecdotes())
   },[dispatch])
 
